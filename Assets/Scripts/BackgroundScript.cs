ff4aad19bab9059cb3f37bcba7989239185cea18
Hugo Rodrigues <hugorodrigues.info@gmail.com>
2017-12-05 15:59:29 +0000

    Teste 2

--- Assets/Scripts/BackgroundScript.cs
+++ Assets/Scripts/BackgroundScript.cs
@@ -12,8 +12,6 @@ public class BackgroundScript : MonoBehaviour {
 		startingPosition = transform.position;
 	}
 
-	// Oi
-
 	private void Update(){
 	
 		transform.Translate ((new Vector3 (0f, -1f, 0f)) * speed * Time.deltaTime);
